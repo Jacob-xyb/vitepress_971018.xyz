9bfc3c1decf268a11989f339aacfacb23dc479cd
Jacob-xyb <949782197@qq.com>
2026-01-30 11:48:24 +0800

    添加本地搜索功能

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,6 +5,26 @@ export default defineConfig({
 	description: '专业的导航网站',
 
 	themeConfig: {
+		search: {
+			provider: 'local',
+			options: {
+				translations: {
+					button: {
+						buttonText: '搜索文档',
+						buttonAriaLabel: '搜索文档'
+					},
+					modal: {
+						noResultsText: '无法找到相关结果',
+						resetButtonTitle: '清除查询条件',
+						footer: {
+							selectText: '选择',
+							navigateText: '切换'
+						}
+					}
+				}
+			}
+		},
+
 		nav: [
 			{ text: '首页', link: '/' },
 			{ text: '指南', link: '/guide/' },
