ba8311394d51a5c46c182b952948107ed7a7711f
Jacob-xyb <949782197@qq.com>
2026-02-03 17:43:16 +0800

    补充仁王3一些小技巧

--- docs/.vitepress/sidebar/walkthrough.js
+++ docs/.vitepress/sidebar/walkthrough.js
@@ -1,2 +1,24 @@
 export default {
+	'/walkthrough/nioh-3/': [
+		{
+			text: 'NIOH 3',
+			items: [
+				{ text: '主页', link: '/walkthrough/nioh-3/' },
+			]
+		},
+		{
+			text: '流派攻略',
+			items: [
+				{
+					text: '武士流派',
+					items: [
+						{ text: '太刀', link: '/walkthrough/nioh-3/武士太刀' }
+					]
+				},
+				{
+					text: '忍者流派',
+				}
+			]
+		}
+	]
 }
