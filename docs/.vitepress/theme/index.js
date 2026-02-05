cea01d66e2ef52704304fba54621c343e8f2ed64
Jacob-xyb <949782197@qq.com>
2026-02-05 11:20:51 +0800

    hero替换成猫咪

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,17 +2,9 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import './markdown.css'
 import ThemeIcon from './components/ThemeIcon.vue'
-import GalaxyBackground from './components/GalaxyBackground.vue'
-import { h } from 'vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // 在主页 hero 的 image 位置插入银河系背景
-      'home-hero-image': () => h(GalaxyBackground)
-    })
-  },
   enhanceApp({ app }) {
     // 注册全局组件
     app.component('ThemeIcon', ThemeIcon)
