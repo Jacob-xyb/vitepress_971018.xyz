45fd87c2cf38330d4fcde55f4791dd4c7fb20b08
Jacob-xyb <949782197@qq.com>
2026-04-10 15:05:03 +0800

    docs: 更新体重数据

--- docs/life/weight-data.js
+++ docs/life/weight-data.js
@@ -2,7 +2,7 @@
  * @Author: Jacob-xyb 949782197@qq.com
  * @Date: 2026-02-02 15:59:37
  * @LastEditors: Jacob-xyb 949782197@qq.com
- * @LastEditTime: 2026-04-01 14:46:42
+ * @LastEditTime: 2026-04-10 15:04:32
  * @Description: Life is struggle.
  */
 export default {
@@ -37,6 +37,12 @@ export default {
     ["2026-03-31", 84.9, null, null],
     ["2026-04-01", 84.9, null, null],
     ["2026-04-02", 84.7, null, null],
+    ["2026-04-03", 84.9, null, null],
+    ["2026-04-04", 84.8, null, null],
+    ["2026-04-07", 84.5, null, null],
+    ["2026-04-08", 84.4, null, null],
+    ["2026-04-09", 84.5, null, null],
+    ["2026-04-10", 84.4, null, null],
     // 在这里添加新数据，可以保留逗号
     // 如果某天没有锻炼，calories 和 rope 填 null，例如：
     // ["2026-02-26", 83.9, null, null],
